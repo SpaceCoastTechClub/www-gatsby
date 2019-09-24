bfd5af26cc7fe621fadaa7462a13e5592fae7366
Gil Creque <gil@codecraftworks.com>
2019-09-24 08:55:32 -0400

    Generate images from event text

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+var fs = require("fs")
+var text2png = require("text2png")
+
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === `meetupEvents`) {
+    const event = node
+    // console.log(`\nEvent: `, event)
+    fs.writeFileSync(
+      "src/images/meetup-events/" + node.id + ".png",
+      text2png(node.name, {
+        font: "80px DankMono",
+        localFontPath: "src/fonts/DankMono-Italic.ttf",
+        localFontName: "DankMono",
+        color: "teal",
+        backgroundColor: "linen",
+        lineSpacing: 10,
+        padding: 20,
+      })
+    )
+  }
+}
