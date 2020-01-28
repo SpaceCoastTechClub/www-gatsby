c6a412d7c39cf667d1029061eaf9b6294d969cf2
Gil Creque <gil@codecraftworks.com>
2020-01-28 09:58:55 -0500

    Added Space Coast Agile Professionals

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         typePrefix: "",
 
         // The url, this should be the endpoint you are attempting to pull data from
-        url: `https://api.meetup.com/2/events?fields=group_photo,series&offset=0&format=json&limited_events=False&group_id=8209012,5292112,27505347,30015014,26468962,8099832,3381942,31892636,17108342,1570697,20150455,28714554,18448785,30790098,31284731,32240720,30816774,1728277,33095463&photo-host=secure&page=500&order=time&desc=false&status=upcoming`,
+        url: `https://api.meetup.com/2/events?fields=group_photo,series&offset=0&format=json&limited_events=False&group_id=8209012,5292112,27505347,30015014,26468962,8099832,3381942,31892636,17108342,1570697,20150455,28714554,18448785,30790098,31284731,32240720,30816774,1728277,33095463,33227599&photo-host=secure&page=500&order=time&desc=false&status=upcoming`,
 
         method: "get",
 
